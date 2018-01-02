8c461b4c988e8385ca23bb1bfe8e19b341e073b9
arnaud ferrand <arferrand@gmail.com>
2018-01-02 15:13:59 +0100

    adding a const file for messages

---
+++ src/const.js
@@ -0,0 +1,11 @@
+const INTERNAL_CONFIG_ERROR = 'Configuration error';
+const INVALID_COORDS_PARAMETERS = 'Invalid coordinates';
+const MISSING_QUERY_PARAMETERS = 'Missing or Invalid input parameters';
+const UNKNOWN_LOCATION = 'Unknown location';
+
+export {
+  INTERNAL_CONFIG_ERROR,
+  INVALID_COORDS_PARAMETERS,
+  MISSING_QUERY_PARAMETERS,
+  UNKNOWN_LOCATION
+};
